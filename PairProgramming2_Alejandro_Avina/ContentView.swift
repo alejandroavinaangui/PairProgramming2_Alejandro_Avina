0bc8b146c2c546582a858c4444b33c76f6353b49
alejandroavinaa <127714455+alejandroavinaa@users.noreply.github.com>
2025-10-11 08:43:11 -0700

    Initial Commit

---
+++ PairProgramming2_Alejandro_Avina/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PairProgramming2_Alejandro_Avina
+//
+//  Created by Alejandro Avina on 10/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PairProgramming2_Alejandro_Avina/PairProgramming2_Alejandro_AvinaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PairProgramming2_Alejandro_AvinaApp.swift
+//  PairProgramming2_Alejandro_Avina
+//
+//  Created by Alejandro Avina on 10/11/25.
+//
+
+import SwiftUI
+
+@main
+struct PairProgramming2_Alejandro_AvinaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
