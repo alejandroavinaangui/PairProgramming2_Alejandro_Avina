c0607490fde0eac4eec16653713c37c76a9584c9
alejandroavinaa <127714455+alejandroavinaa@users.noreply.github.com>
2025-10-11 10:11:22 -0700

    Alejandro Initial

--- PairProgramming2_Alejandro_Avina/ContentView.swift
+++ PairProgramming2_Alejandro_Avina/ContentView.swift
@@ -7,15 +7,49 @@
 
 import SwiftUI
 
+
+let emojis = ["🇺🇸","🇺🇾","🇺🇿","🇻🇺","🇻🇦","🇻🇪","🇻🇳","🇼🇫","🇪🇭","🇾🇪","🇿🇲","🇬🇧"]
+
+private var gridItems = [GridItem(.flexible()),GridItem(.flexible()),GridItem(.flexible())]
+
 struct ContentView: View {
+   
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Text("Game").font(.system(size:40)).foregroundColor(.green).fontWeight(.bold)
+        ScrollView{
+            LazyVGrid(columns:gridItems,spacing:5){
+                ForEach(emojis,id:\.self){
+                    emoji in
+                    EmojiCard(content:emoji)
+                }
+                
+            }
         }
-        .padding()
+    }
+    
+}
+
+struct EmojiCard: View{
+    var content: String;
+    @State var isFaceUp: Bool = true;
+    var body: some View {
+        ZStack{
+            let shape = Rectangle().foregroundColor(.green).frame(width:100);
+            Text(content).font(.system(size:70) ).fontWeight(.bold)
+            if isFaceUp {
+                shape.opacity(0)
+            }
+            else{
+                shape.opacity(1)
+            }
+            
+        }.padding(.horizontal)
+            .onTapGesture {
+                
+                isFaceUp = !isFaceUp
+            }
+      
+        
     }
 }
 
